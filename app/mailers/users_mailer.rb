884484cf1b5f9bf400d20a524b4b6a6e3b3f14f3
Bin Huang <huangbin88@foxmail.com>
2015-04-28 16:27:56 +0800

    Customize default email address for send welcome message.

--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -1,4 +1,6 @@
 class UsersMailer < ApplicationMailer
+  default from: 'no-reply@sample.com'
+
   def welcome_email(user)
     @user = user
     mail(to: @user.email, subject: 'Welcome to Sample App')
