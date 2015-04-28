def98af019acdfb661450a6dce763d0789f27a4b
Bin Huang <huangbin88@foxmail.com>
2015-04-28 16:28:14 +0800

    Default charset of action mailer

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,4 +1,5 @@
 ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.default charset: 'utf-8'
 ActionMailer::Base.smtp_settings = { address: 'smtp.mandrillapp.com',
                                      port: 587,
                                      enable_starttls_auto: true,
