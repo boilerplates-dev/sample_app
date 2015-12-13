5c6cd9de1858d21db855eee6d01b27291c7ea3f5
Bin Huang <huangbin88@foxmail.com>
2015-12-13 22:25:58 +0800

    Upper gems, and fix the broken cap task due to capistrano/rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,8 @@
 lock '3.4.0'
 
-set :rbenv_ruby, "#{File.read('.ruby-version').chomp}"
 set :rbenv_type, :user
+set :rbenv_ruby, "#{File.read('.ruby-version').chomp}"
+set :rbenv_custom_path, '$HOME/.rbenv'  # will be fixed by https://github.com/capistrano/rbenv/pull/59
 
 set :passenger_ruby, -> { "#{fetch(:rbenv_ruby_dir)}/bin/ruby" }
 
@@ -17,10 +18,10 @@ set :deploy_to, -> { "/var/www/#{fetch(:application)}_#{fetch(:stage)}" }
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{.env}
+set :linked_files, %w(.env)
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets}
+set :linked_dirs, %w(bin log tmp/pids tmp/cache tmp/sockets)
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
